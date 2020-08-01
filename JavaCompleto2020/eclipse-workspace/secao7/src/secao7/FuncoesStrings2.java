9b28212aa5faac859d92e1c52400649920cd781d
Miguel Oliveira <oliveirasmj@gmail.com>
2020-08-01 18:37:19 +0100

    Conclusao da secao7

---
+++ JavaCompleto2020/eclipse-workspace/secao7/src/secao7/FuncoesStrings2.java
@@ -0,0 +1,18 @@
+package secao7;
+
+public class FuncoesStrings2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "potato apple lemon";
+		String[] vect = s.split(" ");
+		//String word1 = vect[0];
+		//String word2 = vect[1];
+		//String word3 = vect[2];
+		
+		System.out.println(vect[0]);
+		System.out.println(vect[1]);
+		System.out.println(vect[2]);
+	}
+
+}
