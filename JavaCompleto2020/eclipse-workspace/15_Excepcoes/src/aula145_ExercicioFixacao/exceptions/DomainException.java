c924fc7028cacd84881006107a7e8f9a33747d6d
Miguel Oliveira <oliveirasmj@gmail.com>
2020-09-07 18:54:11 +0100

    Conclusao da secao 15

---
+++ JavaCompleto2020/eclipse-workspace/15_Excepcoes/src/aula145_ExercicioFixacao/exceptions/DomainException.java
@@ -0,0 +1,10 @@
+package aula145_ExercicioFixacao.exceptions;
+
+public class DomainException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public DomainException(String msg) {
+		super(msg);
+	}
+}
