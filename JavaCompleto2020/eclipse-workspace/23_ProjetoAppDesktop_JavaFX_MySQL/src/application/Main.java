544c6a13c7c40adf46ba3070815aabe755cca728
Miguel Oliveira <oliveirasmj@gmail.com>
2020-10-21 17:18:29 +0100

    MainView

--- JavaCompleto2020/eclipse-workspace/23_ProjetoAppDesktop_JavaFX_MySQL/src/application/Main.java
+++ JavaCompleto2020/eclipse-workspace/23_ProjetoAppDesktop_JavaFX_MySQL/src/application/Main.java
@@ -1,25 +1,36 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml")); //instanciar um objeto do tipo FXMLLoader com caminho da View
+			ScrollPane scrollPane = loader.load(); //chamar a View
+			//OU
+			//ScrollPane scrollPane = FXMLLoader.load(getClass().getResource("/gui/MainView.fxml"));
+			
+			scrollPane.setFitToHeight(true); //ajustar janela verticalmente
+			scrollPane.setFitToWidth(true); //ajustar janela horizontalmente
+			
+			Scene mainScene = new Scene(scrollPane); //Criar uma cena
+			primaryStage.setScene(mainScene); //instanciar a cena do palco com argumento da View
+			primaryStage.setTitle("Sample JavaFX application"); //Titulo do palco
+			primaryStage.show(); //Mostrar
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
