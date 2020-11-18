0b8882ab3631a44fc9e2933adfcc52c12124398f
Miguel Oliveira <oliveirasmj@gmail.com>
2020-11-18 17:26:35 +0000

    Eliminacao de user

--- JavaCompleto2020/workspace-spring-tool-suite-4-4.8.1.RELEASE/25_ProjetoSpring/src/main/java/com/example/_ProjetoSpring/resources/UserResource.java
+++ JavaCompleto2020/workspace-spring-tool-suite-4-4.8.1.RELEASE/25_ProjetoSpring/src/main/java/com/example/_ProjetoSpring/resources/UserResource.java
@@ -1,13 +1,18 @@
 package com.example._ProjetoSpring.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example._ProjetoSpring.entities.User;
 import com.example._ProjetoSpring.services.UserService;
@@ -31,4 +36,18 @@ public class UserResource { //classe que disponibiliza um recurso relativamente 
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+		//return ResponseEntity.ok().body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
