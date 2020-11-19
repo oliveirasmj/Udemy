ec94bf782ebc9093285e962ad82885a610ab6bd3
Miguel Oliveira <oliveirasmj@gmail.com>
2020-11-19 22:37:18 +0000

    Deploy app to Heroku

--- JavaCompleto2020/workspace-spring-tool-suite-4-4.8.1.RELEASE/25_ProjetoSpring/src/main/java/com/example/_ProjetoSpring/services/UserService.java
+++ JavaCompleto2020/workspace-spring-tool-suite-4-4.8.1.RELEASE/25_ProjetoSpring/src/main/java/com/example/_ProjetoSpring/services/UserService.java
@@ -3,11 +3,17 @@ package com.example._ProjetoSpring.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example._ProjetoSpring.entities.User;
 import com.example._ProjetoSpring.repositories.UserRepository;
+import com.example._ProjetoSpring.services.exceptions.DatabaseException;
+import com.example._ProjetoSpring.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService { // classe para pesquisar na BD
@@ -21,7 +27,7 @@ public class UserService { // classe para pesquisar na BD
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
@@ -29,13 +35,26 @@ public class UserService { // classe para pesquisar na BD
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
