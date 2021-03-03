603721a8ed73e46fb69fa1f81e9d7b94223de031
Miguel Oliveira <oliveirasmj@gmail.com>
2021-03-03 15:22:00 +0000

    Imutabilidade

---
+++ JavaCompleto2020/eclipse-workspace/Teste/src/teste/Teste.java
@@ -0,0 +1,22 @@
+package teste;
+import java.util.Scanner;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		int num2 = sc.nextInt();
+
+		if(num%2==0) {
+			System.out.println("Par");
+		} else {
+			System.out.println("impar");
+		}
+		
+		sc.close();
+	}
+
+}
