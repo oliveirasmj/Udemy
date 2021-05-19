f9a1efd8dc433cf7212a6d63e16e41a9bbb3fe33
Miguel Oliveira <oliveirasmj@gmail.com>
2021-05-19 01:54:01 +0100

    addPrato Post method

--- JavaCompleto2020/eclipse-workspace/28_WebServices_REST_Server_FolhaTrabalho/src/main/java/entities/Prato.java
+++ JavaCompleto2020/eclipse-workspace/28_WebServices_REST_Server_FolhaTrabalho/src/main/java/entities/Prato.java
@@ -2,43 +2,53 @@ package entities;
 
 public class Prato {
 	
+	private Integer id;
 	private String nome;
 	private Integer quantidade;
 	private Double preco;
 	
-	public Prato(String nome, Integer quantidade, Double preco) {
+	public Prato(Integer id, String nome, Integer quantidade, Double preco) {
+		this.id = id;
 		this.nome = nome;
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
 	public String getNome() {
 		return nome;
 	}
-
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
+	
 	public Integer getQuantidade() {
 		return quantidade;
 	}
-
+	
 	public void setQuantidade(Integer quantidade) {
 		this.quantidade = quantidade;
 	}
-
+	
 	public Double getPreco() {
 		return preco;
 	}
-
+	
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
-
+	
 	@Override
 	public String toString() {
-		return "Prato [nome=" + nome + ", quantidade=" + quantidade + ", preco=" + preco + "]";
+		return "Prato [id=" + id + ", nome=" + nome + ", quantidade=" + quantidade + ", preco=" + preco + "]";
 	}
 	
 }
--- JavaCompleto2020/eclipse-workspace/28_WebServices_REST_Server_FolhaTrabalho/src/main/java/service/rest/PratosService.java
+++ JavaCompleto2020/eclipse-workspace/28_WebServices_REST_Server_FolhaTrabalho/src/main/java/service/rest/PratosService.java
@@ -1,18 +1,23 @@
 package service.rest;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 
 import db.DB;
 import entities.Prato;
@@ -35,8 +40,45 @@ public class PratosService {
 
 			// Imprimir dados
 			while (rs.next()) { // enquanto existir uma proxima linha na tabela rs
-				Prato p = new Prato(rs.getString("nome"), rs.getInt("quantidade"), rs.getDouble("preco"));
-				pratos.add(p);
+				Prato prato = new Prato(rs.getInt("id"), rs.getString("nome"), rs.getInt("quantidade"), rs.getDouble("preco"));
+				pratos.add(prato);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally { // fechar os recursos necessários
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		
+		Gson gson = new Gson();
+		String jsonResp = "";
+		
+		for (Prato p : pratos) {
+			jsonResp += gson.toJson(p);
+			// return pratos.toString(); //Alterar assinatura do metodo para String
+		}
+		return Response.ok(jsonResp, MediaType.APPLICATION_JSON).build();
+//		return null;
+	}
+
+	@GET
+	@Path("/pratos/{id}")
+	public Response pratoID(@PathParam("id") int id) {
+		Connection conn = null; // conectar com BD
+		Statement st = null; // preparar uma consulta sql
+		ResultSet rs = null; // guardar o resultado da consulta
+		List<Prato> pratos = new ArrayList<Prato>();
+
+		try {
+			conn = DB.getConnection(); // conectar com a BD
+			st = conn.createStatement(); // iniciar um statement
+			rs = st.executeQuery("select * from pratos"); // escrever a query e atribuir ao rs
+
+			// Imprimir dados
+			while (rs.next()) { // enquanto existir uma proxima linha na tabela rs
+				Prato prato = new Prato(rs.getInt("id"), rs.getString("nome"), rs.getInt("quantidade"), rs.getDouble("preco"));
+				pratos.add(prato);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -47,10 +89,45 @@ public class PratosService {
 		}
 
 		for (Prato p : pratos) {
-			Gson gson = new Gson();
-			String jsonResp = gson.toJson(p);
-			return Response.ok(jsonResp, MediaType.APPLICATION_JSON).build();
+			if (p.getId() == id) {
+				Gson gson = new Gson();
+				String jsonResp = gson.toJson(p);
+				return Response.ok(jsonResp, MediaType.APPLICATION_JSON).build();
+			}
+
+			// return p.toString(); //Alterar assinatura do metodo para String
 		}
 		return null;
+	}
+	
+	@POST
+	@Path("/addpratos")
+	public void adicionarPrato(@PathParam("nome") String nome, @PathParam("quantidade") int quantidade, @PathParam("preco") double preco) {
+		Connection conn = null; // conectar com BD
+		PreparedStatement st = null; // permite montar consulta SQL
+		
+		try {
+			conn = DB.getConnection();
+			// EXAMPLE 1:
+			st = conn.prepareStatement(
+					"INSERT INTO pratos "
+					+ "(nome, quantidade, preco) "
+					+ "VALUES "
+					+ "(?, ?, ?)");
+			
+			st.setString(1, nome);
+			st.setInt(2, quantidade);
+			st.setDouble(3, preco);
+
+			int rowsAffected = st.executeUpdate(); //saber numero de linhas afetadas e executar
+			System.out.println("Done! Rows affected: " + rowsAffected);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
 	}
 }
